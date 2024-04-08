466a1d9769e11cf51adaf25634677bf5527f130d
mariobassem <mariobassem12@gmail.com>
2024-04-08 03:16:34 +0200

    refactor blob object

--- src/objects/blob.rs
+++ src/objects/blob.rs
@@ -1,62 +1,58 @@
-use std::{
-    fs::{self},
-    path::PathBuf,
-    str::FromStr,
-};
-
-use anyhow::{bail, Result};
-
-use crate::objects::{compress::decompress, ObjectError};
-
-use super::{hash::HashHex, Object, OBJECTS_DIR};
-
-pub fn new_blob(data: Vec<u8>) -> Object {
-    Object::Blob { data }
+pub struct Blob {
+    data: Vec<u8>,
 }
 
-/// read blob contents
-pub fn read_blob(hash_hex: HashHex) -> Result<String> {
-    // get path from hashhex
-    let (dir_name, file_name) = hash_hex.get_object_path();
-
-    // read blob content
-    let file = fs::File::open(PathBuf::from(OBJECTS_DIR).join(dir_name).join(file_name))?;
-
-    // decompress content
-    let decompressed_content = decompress(file)?;
-
-    // parse content
-    let content = parse_blob(decompressed_content)?;
-
-    // return content
-    Ok(content)
+pub fn decode_blob(data: Vec<u8>) -> Blob {
+    Blob { data }
 }
 
-fn parse_blob(data: String) -> Result<String> {
-    let mut rest = data;
-    if !rest.starts_with("blob ") {
-        bail!(ObjectError::ErrParse(String::from(
-            "failed to read 'blob' type"
-        )))
-    }
-
-    let mut rest = rest.split_off("blob ".len());
-    let (size_str, content) = rest
-        .split_once('\0')
-        .ok_or(ObjectError::ErrParse(String::from(
-            "failed to find null byte",
-        )))?;
-
-    // match size to content length
-    let size = size_str.parse::<usize>()?;
-
-    if content.len() != size {
-        bail!(ObjectError::ErrParse(format!(
-            "size is incorrect: found {} expected {}",
-            content.len(),
-            size
-        )))
-    }
-
-    Ok(String::from_str(content)?)
+pub fn encode_blob(blob: Blob) -> Vec<u8> {
+    blob.data
 }
+
+// /// read blob contents
+// pub fn read_blob(hash_hex: HashHex) -> Result<String> {
+//     // get path from hashhex
+//     let (dir_name, file_name) = hash_hex.get_object_path();
+
+//     // read blob content
+//     let file = fs::File::open(PathBuf::from(OBJECTS_DIR).join(dir_name).join(file_name))?;
+
+//     // decompress content
+//     let decompressed_content = decompress(file)?;
+
+//     // parse content
+//     let content = parse_blob(decompressed_content)?;
+
+//     // return content
+//     Ok(content)
+// }
+
+// fn parse_blob(data: String) -> Result<String> {
+//     let mut rest = data;
+//     if !rest.starts_with("blob ") {
+//         bail!(ObjectError::ErrParse(String::from(
+//             "failed to read 'blob' type"
+//         )))
+//     }
+
+//     let mut rest = rest.split_off("blob ".len());
+//     let (size_str, content) = rest
+//         .split_once('\0')
+//         .ok_or(ObjectError::ErrParse(String::from(
+//             "failed to find null byte",
+//         )))?;
+
+//     // match size to content length
+//     let size = size_str.parse::<usize>()?;
+
+//     if content.len() != size {
+//         bail!(ObjectError::ErrParse(format!(
+//             "size is incorrect: found {} expected {}",
+//             content.len(),
+//             size
+//         )))
+//     }
+
+//     Ok(String::from_str(content)?)
+// }
