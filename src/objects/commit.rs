e25ae4af331cdf73f7520793384faecd9073acd9
mariobassem <mariobassem12@gmail.com>
2024-04-12 18:57:03 +0200

    implement commit encode

--- src/objects/commit.rs
+++ src/objects/commit.rs
@@ -55,8 +55,47 @@ pub fn decode_commit(mut data: Vec<u8>) -> Result<Commit> {
     })
 }
 
-pub fn encode_commit(commit: Commit) -> Vec<u8> {
-    todo!()
+pub fn encode_commit(commit: Commit) -> Result<Vec<u8>> {
+    let mut content = Vec::new();
+
+    content.append(&mut "tree ".as_bytes().to_vec());
+    content.append(&mut commit.tree.0.into_bytes());
+    content.append(&mut "\n".as_bytes().to_vec());
+
+    for parent in commit.parents {
+        let parent_hash = parent.get_hash()?;
+        content.append(&mut "parent ".as_bytes().to_vec());
+        content.append(&mut parent_hash.into());
+        content.append(&mut "\n".as_bytes().to_vec());
+    }
+
+    content.append(
+        &mut format!(
+            "author {} <{}> {} {}\n",
+            commit.author.name, commit.author.email, commit.author.time, commit.author.time_zone
+        )
+        .into_bytes(),
+    );
+    content.append(
+        &mut format!(
+            "committer {} <{}> {} {}\n",
+            commit.committer.name,
+            commit.committer.email,
+            commit.committer.time,
+            commit.committer.time_zone
+        )
+        .into_bytes(),
+    );
+
+    if let Some(additional_data) = commit.additional_data {
+        content.append(&mut format!("{}\n", additional_data).into_bytes())
+    }
+
+    content.append(&mut "\n".as_bytes().to_vec());
+    content.append(&mut commit.message.into_bytes());
+    content.append(&mut "\n".as_bytes().to_vec());
+
+    Ok(content)
 }
 
 fn get_tree_hash_hex(data: &mut Vec<u8>) -> Result<HashHex> {
@@ -195,57 +234,3 @@ fn get_char(data: &mut Vec<u8>, b: u8) -> Result<()> {
 
     Ok(())
 }
-
-// pub fn write_commit(
-//     tree_hash_hex: HashHex,
-//     parents: Vec<HashHex>,
-//     author: Author,
-//     message: String,
-// ) -> Result<Hash> {
-//     // TODO: validate tree exists
-//     // TODO: validate parent commits exist
-
-//     // create content
-//     let mut content = Vec::new();
-//     let tree_hash = tree_hash_hex.get_hash()?;
-//     content.append(&mut "tree ".as_bytes().to_vec());
-//     content.append(&mut tree_hash.into());
-
-//     for parent in parents {
-//         let parent_hash = parent.get_hash()?;
-//         content.append(&mut "parent ".as_bytes().to_vec());
-//         content.append(&mut parent_hash.into());
-//     }
-
-//     content.append(
-//         &mut format!(
-//             "author {} <{}> {} {}",
-//             author.name, author.email, author.time, author.time_zone
-//         )
-//         .as_bytes()
-//         .to_vec(),
-//     );
-//     content.append(
-//         &mut format!(
-//             "committer {} <{}> {} {}",
-//             author.name, author.email, author.time, author.time_zone
-//         )
-//         .as_bytes()
-//         .to_vec(),
-//     );
-
-//     content.append(&mut "\n".as_bytes().to_vec());
-//     content.append(&mut message.as_bytes().to_vec());
-
-//     let mut commit_object_content = format!("commit {}\0", content.len()).as_bytes().to_vec();
-//     commit_object_content.append(&mut content);
-
-//     // compress
-//     let compressed_object = compress(&commit_object_content)?;
-
-//     // write object
-//     let hash = write_object(compressed_object)?;
-
-//     // return hash
-//     Ok(hash)
-// }
