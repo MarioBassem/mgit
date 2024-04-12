d5badead1ef3787e58d3f52fc3f414ca05fde4b5
mariobassem <mariobassem12@gmail.com>
2024-04-12 20:00:27 +0200

    implement tag encode/decode

--- src/objects/tag.rs
+++ src/objects/tag.rs
@@ -1,7 +1,16 @@
-use bytes::Bytes;
+use std::io::BufRead;
 
-use super::{commit::Author, hash::HashHex, Object, ObjectKind, ObjectTrait};
-use anyhow::Result;
+use super::{commit::Author, hash::HashHex, Object, ObjectKind};
+use anyhow::{anyhow, Result};
+
+pub struct Tag {
+    object: HashHex,
+    object_type: ObjectKind,
+    tag_name: String,
+    tagger: Author,
+    commit_message: Option<String>,
+    additional_data: Option<String>,
+}
 
 pub fn new_tag(
     object: HashHex,
@@ -9,22 +18,76 @@ pub fn new_tag(
     tag_name: String,
     tagger: Author,
     commit_message: Option<String>,
-    signature: Option<String>,
+    additional_data: Option<String>,
 ) -> Object {
-    todo!()
-}
-
-pub fn parse_tag(data: Vec<u8>) -> Result<Object> {
     todo!()
 }
 
 /*
     format:
-        tag size NUL object object_hex_hash LF
+        tag size NUL
+        object object_hex_hash LF
         type object_type LF
         tag tag_name LF
-        tagger author LF LF
+        tagger author LF
+        additional_data LF
+        LF
         commit_message LF
-        signature
 
 */
+
+pub fn decode_tag(data: Vec<u8>) -> Result<Tag> {
+    let mut tag: Tag;
+    let lines: Vec<String> = data.lines().collect::<Result<_, _>>()?;
+    for (i, line) in lines.iter().enumerate() {
+        if line.len() == 0 {
+            // next is commit message
+            if i != lines.len() - 2 {
+                return Err(anyhow!("invalid tag data"));
+            }
+
+            tag.commit_message = Some(lines[i + 1]);
+            break;
+        }
+
+        let (first_word, words) = line.split_once(' ').ok_or(anyhow!("invalid tag data"))?;
+        match first_word {
+            "object" => tag.object = HashHex(String::from(words)),
+            "type" => tag.object_type = ObjectKind::try_from(words)?,
+            "tag" => tag.tag_name = String::from(words),
+            "tagger" => tag.tagger = Author::try_from(words)?,
+            _ => tag.additional_data = Some(line.to_string()),
+        }
+    }
+
+    Ok(tag)
+}
+
+pub fn encode_tag(tag: Tag) -> Vec<u8> {
+    let mut content = Vec::new();
+
+    content.append(&mut format!("object {}\n", tag.object.0).into_bytes());
+
+    content.append(&mut format!("type {}\n", tag.object_type).into_bytes());
+
+    content.append(&mut format!("tag {}\n", tag.tag_name).into_bytes());
+
+    content.append(
+        &mut format!(
+            "tagger {} <{}> {} {}\n",
+            tag.tagger.name, tag.tagger.email, tag.tagger.time, tag.tagger.time_zone
+        )
+        .into_bytes(),
+    );
+
+    if let Some(additional_data) = tag.additional_data {
+        content.append(&mut format!("{}\n", additional_data).into_bytes());
+    }
+
+    content.append(&mut "\n".as_bytes().to_vec());
+    if let Some(message) = tag.commit_message {
+        content.append(&mut format!("{}\n", message).into_bytes())
+    }
+
+    content
+}
