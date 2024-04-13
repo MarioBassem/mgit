60a5bb9982be90752212faecf3c6db8ced70c359
mariobassem <mariobassem12@gmail.com>
2024-04-13 04:55:40 +0200

    clean tree implementation

--- src/objects/tree.rs
+++ src/objects/tree.rs
@@ -1,4 +1,4 @@
-use super::{hash::Hash, ObjectError};
+use super::{hash::Hash, Object, ObjectError};
 use anyhow::{anyhow, bail, Ok, Result};
 use std::{fmt::Display, fs::DirEntry, os::unix::fs::PermissionsExt, str::FromStr};
 
@@ -13,21 +13,18 @@ pub fn new_tree(entries: Vec<Entry>) -> Tree {
 pub fn decode_tree(mut data: Vec<u8>) -> Result<Tree> {
     let mut entries = Vec::<Entry>::new();
     while data.len() > 0 {
-        let mut tree_entry_info: String;
-        let mut hash: Vec<u8>;
-        for (i, b) in data.iter().enumerate() {
-            if *b == b'\0' {
-                tree_entry_info = String::from_utf8(data.split_off(i))?;
-                data.split_off(1);
-                hash = data.split_off(20);
-                break;
-            }
-        }
+        let null_byte_index = data
+            .iter()
+            .position(|c| *c == b'\0')
+            .ok_or(anyhow!("invalid tree entry data"))?;
+        let mut tree_entry_info = String::from_utf8(data[..null_byte_index].to_vec())?;
+        data.drain(0..null_byte_index + 1);
+        let mut hash = data.split_off(20);
 
         let (mode_str, name_str) = tree_entry_info
             .split_once(' ')
             .ok_or(anyhow!("invalid tree entry information"))?;
-        let mode = get_mode_from_bytes(mode_str)?;
+        let mode = EntryMode::try_from(mode_str)?;
 
         entries.push(Entry {
             hash: Hash::try_from(hash.as_ref())?,
@@ -70,10 +67,8 @@ impl Entry {
 
         let os_name = dir_entry.file_name();
 
-        let hash = match mode {
-            EntryMode::Directory => write_tree(dir_entry.path())?,
-            _ => write_blob(dir_entry.path())?,
-        };
+        let object = Object::read_from_path(dir_entry.path())?;
+        let hash = object.hash()?;
 
         let name = os_name
             .into_string()
@@ -113,121 +108,18 @@ impl Display for EntryMode {
     }
 }
 
-// pub fn write_tree(path: PathBuf) -> Result<Hash> {
-//     // list current dir entries
-//     let paths = fs::read_dir("./")?;
+impl TryFrom<&str> for EntryMode {
+    type Error = anyhow::Error;
+    fn try_from(value: &str) -> std::prelude::v1::Result<Self, Self::Error> {
+        let mode = usize::from_str_radix(value, 8)?;
+        let entry_mode = match mode {
+            0o40000 => EntryMode::Directory,
+            0o120000 => EntryMode::SymbolicLink,
+            0o100755 => EntryMode::ExecutableFile,
+            0o100644 => EntryMode::RegularFile,
+            _ => bail!(ObjectError::ErrInvalidMode(mode)),
+        };
 
-//     // for each entry call its write method
-//     let mut children_content = Vec::new();
-//     for path in paths {
-//         let dir_entry = path?;
-//         let entry = Entry::from_dir_entry(dir_entry)?;
-//         children_content.append(&mut entry.into());
-//     }
-
-//     // generate tree file content
-//     let mut tree_object_content = format!("tree {}\0", children_content.len())
-//         .as_bytes()
-//         .to_vec();
-
-//     tree_object_content.append(&mut children_content);
-//     // compress content
-//     let compressed_content = compress(tree_object_content)?;
-
-//     // write object
-//     let hash = write_object(compressed_content)?;
-
-//     // return hash
-//     Ok(hash)
-// }
-
-// pub fn read_tree(hash_hex: HashHex) -> Result<Vec<Entry>> {
-//     // get path from hashhex
-//     let (dir_name, file_name) = hash_hex.get_object_path();
-
-//     // read tree content
-//     let file = fs::File::open(PathBuf::from(OBJECTS_DIR).join(dir_name).join(file_name))?;
-
-//     // decompress
-//     let decompressed_content = decompress(file)?;
-
-//     // parse
-//     let entries = decode_tree(decompressed_content)?;
-
-//     // return list of entries
-//     Ok(entries)
-// }
-
-// fn parse_tree(data: String) -> Result<Vec<Entry>> {
-//     let mut rest = data;
-//     if !rest.starts_with("tree ") {
-//         bail!(ObjectError::ErrParse(String::from(
-//             "failed to read 'tree' type"
-//         )))
-//     }
-
-//     let rest = rest.split_off("blob ".len());
-//     let (size_str, content) = rest
-//         .split_once('\0')
-//         .ok_or(ObjectError::ErrParse(String::from(
-//             "failed to find null byte",
-//         )))?;
-
-//     // match size to content length
-//     let size = size_str.parse::<usize>()?;
-
-//     if content.len() != size {
-//         bail!(ObjectError::ErrParse(format!(
-//             "size is incorrect: found {} expected {}",
-//             content.len(),
-//             size
-//         )))
-//     }
-
-//     let mut read_bytes = 0;
-//     let mut entries = Vec::<Entry>::new();
-//     while read_bytes < size {
-//         let (mode_str, content) = content
-//             .split_once(' ')
-//             .ok_or(ObjectError::ErrParse(format!(
-//                 "failed to parse tree entry mode"
-//             )))?;
-
-//         read_bytes += mode_str.len() + 1;
-
-//         let mode = get_mode_from_bytes(mode_str)?;
-
-//         let (name_str, content) =
-//             content
-//                 .split_once('\0')
-//                 .ok_or(ObjectError::ErrParse(format!(
-//                     "failed to parse tree entry name"
-//                 )))?;
-
-//         read_bytes += name_str.len() + 1;
-
-//         let (hash_str, content) = content.split_at(20);
-//         read_bytes += hash_str.len();
-
-//         entries.push(Entry {
-//             hash: Hash::from(hash_str),
-//             mode,
-//             name: String::from_str(name_str)?,
-//         });
-//     }
-
-//     Ok(entries)
-// }
-
-fn get_mode_from_bytes(mode_str: &str) -> Result<EntryMode> {
-    let mode = usize::from_str_radix(mode_str, 8)?;
-    let entry_mode = match mode {
-        0o40000 => EntryMode::Directory,
-        0o120000 => EntryMode::SymbolicLink,
-        0o100755 => EntryMode::ExecutableFile,
-        0o100644 => EntryMode::RegularFile,
-        _ => bail!(ObjectError::ErrInvalidMode(mode)),
-    };
-
-    Ok(entry_mode)
+        Ok(entry_mode)
+    }
 }
