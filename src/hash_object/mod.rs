cc8374f6e788bd05e5142d005812e14508ea5020
mario <mariobassem12@gmail.com>
2024-04-19 23:32:18 +0200

    implement hash_object command

--- src/hash_object/mod.rs
+++ src/hash_object/mod.rs
@@ -1,7 +1,29 @@
-use std::path::PathBuf;
+use std::{fs, io::Read, path::PathBuf};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use log::info;
+
+use crate::objects::{hash::Hash, Object, ObjectKind};
 
 pub fn hash_object(path: PathBuf, write: bool) -> Result<()> {
-    todo!()
+    if !path.is_file() {
+        return Err(anyhow!("path is not a file"));
+    }
+
+    let mut file_contents = fs::File::open(path)?;
+    let mut data = Vec::new();
+    file_contents.read(&mut data)?;
+
+    let object = Object::new(data, ObjectKind::Blob);
+
+    let hash: Hash;
+    if write {
+        hash = object.write()?;
+    } else {
+        hash = object.hash()?;
+    }
+
+    info!("{:x}", hash);
+
+    Ok(())
 }
